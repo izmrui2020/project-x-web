ea86b1f7eda40ad1ae5ef1bd899d867721ade4f0
rr-rr-re <rr.anonymous.2020@gmail.com>
2021-06-06 15:34:04 +0900

    save point

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { SignUpComponent } from './account/sign-up/sign-up.component';
+import { LoginComponent } from './account/login/login.component';
 import { ExceptionPageComponent } from './error/exception-page/exception-page.component';
 import { PageNotFoundComponent } from './error/page-not-found/page-not-found.component';
 
@@ -10,6 +11,7 @@ import { PageNotFoundComponent } from './error/page-not-found/page-not-found.com
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'sign-up', component: SignUpComponent },
   { path: 'opps!', component: ExceptionPageComponent },
   { path: '404', component: PageNotFoundComponent },
