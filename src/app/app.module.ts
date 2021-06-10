620dea97e6ce0d728cf3fbfb61f8133f4295e608
rr-rr-re <rr.anonymous.2020@gmail.com>
2021-06-10 22:55:54 +0900

    setting http service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-//import { SidebarModule } from 'ng-sidebar';
+import { HttpService } from './service/http.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,7 +27,7 @@ import { HomeModule } from './home/home.module'
     HomeModule,
  //   SidebarModule.forRoot()
   ],
-  providers: [],
+  providers: [HttpService],
   bootstrap: [AppComponent],
 
 })
