03af2df3e56484bbcd4e9a4176cb3323fd1b51a3
rr-rr-re <rr.anonymous.2020@gmail.com>
2021-06-27 20:38:20 +0900

    save comminication rails

--- src/app/home/oogiri/oogiri.service.ts
+++ src/app/home/oogiri/oogiri.service.ts
@@ -1,20 +1,33 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, of } from 'rxjs'
 import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { map, catchError } from 'rxjs/operators';
 
 import { Oogiri } from '../oogiri/oogiri';
+import { environment } from '../../../environments/environment';
+import { HttpService } from '../../service/http.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OogiriService {
+  private hosturi = environment.uri; //url: 'http://localhost:3000'
 
   constructor(
     private http: HttpClient,
+    private htservie: HttpService,
   ) { }
 
   getOogiries(): Observable<Oogiri[]> {
-    return
+    return this.http.get(this.hosturi + "/oogiris").pipe(
+      map((res = Response) => {
+        return <Oogiri[]>res;
+      }),
+      //catchError(this.htservie.handleError)
+    )
+
   }
+  // .map((res = Response) => <Oogiri[]>res.json())
+  // .catchError(this.htservie.handleError);
 
 }
--- src/app/service/http.service.ts
+++ src/app/service/http.service.ts
@@ -31,7 +31,7 @@ export class HttpService {
  * @param result - observableな結果として返す任意の値
  * 失敗したHttp操作を処理します。
  * アプリを持続させます。*/
-  handleError<T>(operation = 'operation', result?: T) {
+  public handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
 
       //TODO: リモート上のロギング基盤にエラーを送信する
