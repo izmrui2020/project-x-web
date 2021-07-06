50fd8178647a4688e8d9868ff15c35bacffaefbd
rr-rr-re <rr.anonymous.2020@gmail.com>
2021-07-07 02:39:38 +0900

    setting amplify

--- src/app/account/sign-up/sign-up.component.ts
+++ src/app/account/sign-up/sign-up.component.ts
@@ -25,25 +25,25 @@ export class SignUpComponent implements OnInit {
   ) { }
 
   onSubmitSignup(value: any) {
-    const email = value.email, password = value.password;
-    this.cognito.signUp(email, password)
-    .then((result) => {
-      console.log(result);
-      this.successfullySignup = true;
-    }).catch((err) => {
-      console.log(err);
-    });
+    // const email = value.email, password = value.password;
+    // this.cognito.signUp(email, password)
+    // .then((result) => {
+    //   console.log(result);
+    //   this.successfullySignup = true;
+    // }).catch((err) => {
+    //   console.log(err);
+    // });
   }
 
   onSubmitConfirmation(value: any) {
-    const email = value.email, confirmationCode = value.confirmationCode;
-    console.log(email);
-    this.cognito.confirmation(email, confirmationCode)
-    .then((result) => {
-      return console.log(result) || this.router.navigate(['/login']);
-    }).catch((err) => {
-      console.log(err);
-    });
+    // const email = value.email, confirmationCode = value.confirmationCode;
+    // console.log(email);
+    // this.cognito.confirmation(email, confirmationCode)
+    // .then((result) => {
+    //   return console.log(result) || this.router.navigate(['/login']);
+    // }).catch((err) => {
+    //   console.log(err);
+    // });
   }
 
   ngOnInit(): void {
