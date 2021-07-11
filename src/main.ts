4044f3c9e3246c90a89c2d745f9091bcf732d7c1
rr-rr-re <rr.anonymous.2020@gmail.com>
2021-07-11 23:48:36 +0900

    cognito config

--- src/main.ts
+++ src/main.ts
@@ -4,24 +4,24 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import Amplify, { Auth } from 'aws-amplify';
-import amplify from './aws-exports';
-Amplify.configure(amplify);
+// import Amplify, { Auth } from 'aws-amplify';
+// import amplify from './aws-exports';
+// Amplify.configure(amplify);
 
-const oauth = {
-  domain : 'oogiri-auth.auth.ap-northeast-1.amazoncognito.com',
-  scope : ['phone', 'email', 'profile', 'openid','aws.cognito.signin.user.admin'],
-  redirectSignIn : 'http://localhost:4200/home',
-  redirectSignOut : 'http://localhost:4200/home',
-  responseType: 'code',
-  options: {
-      AdvancedSecurityDataCollectionFlag : false
-  }
-}
+// const oauth = {
+//   domain : 'oogiri-auth.auth.ap-northeast-1.amazoncognito.com',
+//   scope : ['phone', 'email', 'profile', 'openid','aws.cognito.signin.user.admin'],
+//   redirectSignIn : 'http://localhost:4200/home',
+//   redirectSignOut : 'http://localhost:4200/home',
+//   responseType: 'code',
+//   options: {
+//       AdvancedSecurityDataCollectionFlag : false
+//   }
+// }
 
-Auth.configure({
-  oauth:oauth
-});
+// Auth.configure({
+//   oauth:oauth
+// });
 
 if (environment.production) {
   enableProdMode();
