0defad79ddbd54241e9a52d1b6105e308d0b4f4a
rr-rr-re <rr.anonymous.2020@gmail.com>
2021-07-12 23:44:58 +0900

    add cognito serice

--- src/app/account/cognito.service.ts
+++ src/app/account/cognito.service.ts
@@ -1,22 +1,41 @@
 import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environment'
 
+import { Router } from '@angular/router';
+import { Observable, of } from 'rxjs'
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { fromPromise } from 'rxjs/observable/fromPromise';
+import { map, tap, catchError } from 'rxjs/operators';
+//setting cognito
+import Amplify, {Auth} from 'aws-amplify';
+import awsconfig from '../../aws-exports';
+/* Configure Amplify resources */
+
+
+import { environment } from '../../environments/environment'
 
 @Injectable({
   providedIn: 'root'
 })
 export class CognitoService {
-  private poolData: any;
-  public cognitoCreds: AWS.CognitoIdentityCredentials;
 
-  constructor() {}
+  public loggedIn: BehaviorSubject<boolean>;
+
+  constructor(
+    private router: Router
+  ) {
+    Amplify.configure(awsconfig);
+    this.loggedIn = new BehaviorSubject<boolean>(false);
+  }
 
 /**
  *
- *
+ *signUp Method
  *
  *
  * */
+  public signUp(email, password): Observable<any> {
+  return fromPromise(Auth.signUp(email, password));
+  }
 
 /**
  *
