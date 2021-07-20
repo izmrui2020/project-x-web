d49d6cc385b9eaf9ba28423f447348d3d67392d1
rr-rr-re <rr.anonymous.2020@gmail.com>
2021-07-21 01:51:06 +0900

    model chenge

--- src/app/home/oogiri/oogiri-list/oogiri-list.component.ts
+++ src/app/home/oogiri/oogiri-list/oogiri-list.component.ts
@@ -11,20 +11,21 @@ import { CognitoService } from '../../../account/cognito.service';
   templateUrl: './oogiri-list.component.html',
   styleUrls: ['./oogiri-list.component.scss']
 })
+///tep data
 export class OogiriListComponent implements OnInit {
-  tmpOne = new Oogiri(1, 'title1', 'oogiri1', 'comment1', 'https://sample.com')
-	tmpTwo: Oogiri = new Oogiri(2, 'title2', 'oogiri2', 'comment2', 'https://sample.com')
-	tmpThree: Oogiri = new Oogiri(3, 'title3', 'oogiri3', 'comment3', 'https://sample.com')
 
-	temps : Oogiri[] = [
+  tmpOne: Oogiri = new Oogiri(1, 'title1', 'oogiri1', 'comment1', 'https://oogiri-images.s3.ap-northeast-1.amazonaws.com/test/sample5.png')
+	tmpTwo: Oogiri = new Oogiri(2, 'title2', 'oogiri2', 'comment2', 'https://oogiri-images.s3.ap-northeast-1.amazonaws.com/test/sample5.png')
+	tmpThree: Oogiri = new Oogiri(3, 'title3', 'oogiri3', 'comment3', 'https://oogiri-images.s3.ap-northeast-1.amazonaws.com/test/sample5.png')
+
+	oogiries : Oogiri[] = [
 		this.tmpOne,
 		this.tmpTwo,
 		this.tmpThree
 	]
 
-
   private token: string;
-  oogiries: Oogiri[];
+  //oogiries: Oogiri[];
   errorMessage: string;
 
   constructor(
@@ -33,11 +34,13 @@ export class OogiriListComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    let timer = interval(1000);
-    timer.subscribe(() => this.getOogiries());
+    // let timer = interval(1000);
+    // timer.subscribe(() => this.getOogiries());
+    this.getOogiries()
   }
 
   getOogiries() {
+    return this.oogiries
     // this._os.getOogiries()
     //   .subscribe(
     //     result => {
--- src/app/home/oogiri/oogiri-model.ts
+++ src/app/home/oogiri/oogiri-model.ts
@@ -1,9 +1,20 @@
-export interface Oogiri {
-  id: number,
-	title: string,
-  oogiri: string,
-	description: string,
-	// file_url: string, //後で消す。
-	// updated_at: string,
-	image_url: string,
+// export interface Oogiri {
+//   id: number,
+// 	title: string,
+//   oogiri: string,
+// 	description?: string,
+//   //誰が作ったのか？
+// 	// file_url: string, //後で消す。
+// 	// updated_at: string,
+// 	image_url: string,
+// }
+
+export class Oogiri {
+	constructor(
+		public id: number,
+		public title: string,
+		public oogiri: string,
+		public description?: string,
+    public image_url?: string,
+	) {}
 }
