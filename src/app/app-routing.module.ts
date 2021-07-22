d725f583be95854b05c3804b75004d4b22155df9
rr-rr-re <rr.anonymous.2020@gmail.com>
2021-07-22 17:59:49 +0900

    add error routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,8 @@ import { TopPageComponent } from './top-page/top-page.component';
 import { LoginComponent } from './account/login/login.component';
 import { SignUpComponent } from './account/sign-up/sign-up.component';
 
+import { NotFoundComponent } from './error/not-found/not-found.component';
+import { ExceptionErrorComponent } from './error/exception-error/exception-error.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/top', pathMatch: 'full' },
@@ -13,7 +15,9 @@ const routes: Routes = [
   { path: 'signup', component: SignUpComponent },
   { path: 'home',
   loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
-  }
+  },
+  { path: '404', component: NotFoundComponent },
+  { path: '', component: ExceptionErrorComponent }
 ];
 
 @NgModule({
