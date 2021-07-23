2164b14d3d93860ba631a763a39a6d935b8db837
rr-rr-re <rr.anonymous.2020@gmail.com>
2021-07-23 22:36:42 +0900

    user component

--- src/app/home/user/user/user.component.ts
+++ src/app/home/user/user/user.component.ts
@@ -10,8 +10,17 @@ import { CognitoService } from '../../../account/cognito.service';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
+  tmpOne: Oogiri = new Oogiri(1, 'title1', 'oogiri1', 'comment1', 'https://oogiri-images.s3.ap-northeast-1.amazonaws.com/test/sample5.png')
+	tmpTwo: Oogiri = new Oogiri(2, 'title2', 'oogiri2', 'comment2', 'https://oogiri-images.s3.ap-northeast-1.amazonaws.com/test/sample5.png')
+	tmpThree: Oogiri = new Oogiri(3, 'title3', 'oogiri3', 'comment3', 'https://oogiri-images.s3.ap-northeast-1.amazonaws.com/test/sample5.png')
 
-  myOogiries: Oogiri[];
+	myOogiries : Oogiri[] = [
+		this.tmpOne,
+		this.tmpTwo,
+		this.tmpThree
+	]
+
+  //myOogiries: Oogiri[];
 
   constructor() { }
 
