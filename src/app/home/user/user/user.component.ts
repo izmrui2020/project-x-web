6eb85d458f9e080b6d9e770a26934a361cb3583a
rr-rr-re <rr.anonymous.2020@gmail.com>
2021-07-23 22:06:05 +0900

    user oogiriees

--- src/app/home/user/user/user.component.ts
+++ src/app/home/user/user/user.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Oogiri } from '../../oogiri/oogiri-model';
+import { UserService } from '../user.service';
+import { CognitoService } from '../../../account/cognito.service';
+
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
+
+  myOogiries: Oogiri[];
 
   constructor() { }
 
